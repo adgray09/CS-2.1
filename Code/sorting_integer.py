5fff5001c1297170cb7ff5a8501777acd75b34a7
GraySpace <alexandergray37@gmail.com>
2020-11-09 16:39:49 -0500

    finished bucket sort

--- Code/sorting_integer.py
+++ Code/sorting_integer.py
@@ -13,6 +13,13 @@ def counting_sort(numbers):
     # FIXME: Improve this to mutate input instead of creating new output list
 
 
+def bubble_sort(numbers):
+    for i in range(0, len(numbers)-1):
+        for j in range(0 , len(numbers)-1):
+            if numbers[j] > numbers[j+1]:
+                numbers[i], numbers[j+1] = numbers[j+1], numbers[i]
+    return numbers
+
 def bucket_sort(numbers, num_buckets=10):
     """Sort given numbers by distributing into buckets representing subranges,
     then sorting each bucket and concatenating all buckets in sorted order.
@@ -23,4 +30,46 @@ def bucket_sort(numbers, num_buckets=10):
     # TODO: Loop over given numbers and place each item in appropriate bucket
     # TODO: Sort each bucket using any sorting algorithm (recursive or another)
     # TODO: Loop over buckets and append each bucket's numbers into output list
-    # FIXME: Improve this to mutate input instead of creating new output list
+    # FIXME: Improve this to mutate input instead of creating new output lis
+    
+    max_value = max(numbers)
+    bucket_list = []
+    size = max_value/len(numbers)
+    result = []
+    
+    for i in range(num_buckets):
+        bucket_list.append([])
+        
+    for i in range(len(numbers)):
+        j = int(numbers[i] / size)
+        
+        if j != len(numbers):
+            bucket_list[j].append(numbers[i])
+        else:
+            bucket_list[len(numbers)-1].append(numbers[i])
+            
+    for z in range(len(numbers)):
+        bubble_sort(bucket_list[z])
+        
+    for x in range(len(numbers)):
+        result = result + bucket_list[x]
+    return result
+
+items = [4,3,6,9,8,1,2]
+
+print(bucket_sort(items)) 
+
+def bucket_sort(numbesr, num_buckets=10):
+    max_value = max(numbers)
+    size = max_size/len(numbers)
+    result = []
+    bucket_list = []
+    
+    for i in range(num_buckets):
+        bucket_list.append([])
+        
+    for i in range(len(numbers)):
+        j = int(numbers[i]/size)
+        
+        if j != len(numbers):
+            bucket_list[j].append(numbers[i])          
